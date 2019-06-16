40361a1387a489f8444d5eaa1f992864515c8742
iamgabrielma <gabrielmaldonado.art@gmail.com>
2019-06-16 14:04:52 +0700

    Initial commit & Part 0 & Part 1 completed
    - Part 0: Initial Setup
    - Part 1: Drawing the ‘@’ symbol and moving it around

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    // Temporary screen_width & height
+    readonly int screen_width = 80;
+    readonly int screen_height = 50;
+
+    public static GameObject __player; // TODO -> Question: Is using a static gameobject for player a good idea?
+
+    public void Start()
+    {
+        // Coordinates where we'll instantiate the player.
+        //TODO: Is not clear at the moment if we will use these. Revisit and pass (0,0,0) vector directly to the __player instantiation otherwise
+        int player_x = 0;
+        int player_y = 0;
+
+        // The playerObject is assigned and the __player instance is created
+        GameObject playerObject = Resources.Load<GameObject>("Prefabs/Player"); 
+        __player = Instantiate(playerObject, new Vector3(player_x, player_y, 0), Quaternion.identity);
+
+    }
+
+
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private bool isPlayerMoving; // This avoids that there's multiple movement until the previous movement hasn't completed
+
+    GameObject player; // Self reference, in order to use MovePlayer() easily
+
+    private void Start()
+    {
+        // TODO: add nullcheck. We assign player to the static __player instance, shouldn't be null but check for safety.
+        player = Engine.__player;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isPlayerMoving == false)
+        {
+            isPlayerMoving = true;
+            MovePlayer("up");
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && isPlayerMoving == false)
+        {
+            isPlayerMoving = true;
+            MovePlayer("down");
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && isPlayerMoving == false)
+        {
+            isPlayerMoving = true;
+            MovePlayer("right");
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && isPlayerMoving == false)
+        {
+            isPlayerMoving = true;
+            MovePlayer("left");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // TODO: Will implement game menu screen here in the future.
+        }
+
+    }
+
+    public void MovePlayer(string direction)
+    {
+        switch (direction)
+        {
+            case "up":
+                float yPositive = player.transform.localPosition.y + 1.0f;
+                player.transform.localPosition = new Vector3(player.transform.localPosition.x, yPositive, 0);
+                break;
+            case "down":
+                float yNegative = player.transform.localPosition.y - 1.0f;
+                player.transform.localPosition = new Vector3(player.transform.localPosition.x, yNegative, 0);
+                break;
+            case "right":
+                float xPositive = player.transform.localPosition.x + 1.0f;
+                player.transform.localPosition = new Vector3(xPositive, player.transform.localPosition.y, 0);
+                break;
+            case "left":
+                float xNegative = player.transform.localPosition.x - 1.0f;
+                player.transform.localPosition = new Vector3(xNegative, player.transform.localPosition.y, 0);
+                break;
+
+        }
+        isPlayerMoving = false;
+    }
+}
